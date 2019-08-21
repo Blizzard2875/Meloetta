f73dc58aa453ae457639fdf13d5198e327b3d0e7
Josh B <josh.ja.butt@gmail.com>
2019-08-21 17:19:44 +1000

    Set requester on treasure town requests.

--- bot/cogs/player/__init__.py
+++ bot/cogs/player/__init__.py
@@ -80,9 +80,10 @@ class Player(commands.Cog):
         """
         await ctx.invoke(self.request, request=request)
 
+    # War related code - Will be deleted
     @request.command(name='map', hidden=True)
     async def request_map(self, ctx: commands.Context):
-        request = MP3Track(COG_CONFIG.DEFAULT_PLAYLIST_DIRECTORY + 'mystery_dungeon_time_darkness_sky/014 Treasure Town.mp3')
+        request = MP3Track(COG_CONFIG.DEFAULT_PLAYLIST_DIRECTORY + 'mystery_dungeon_time_darkness_sky/014 Treasure Town.mp3', requester=ctx.user)
         await ctx.invoke(self.request, request=request)
 
     @commands.command(name="skip")
