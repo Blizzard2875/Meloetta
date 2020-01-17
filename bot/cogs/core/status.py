fbff4585a40e6a5ab61cb8d75c47ab54c58fd585
Josh B <josh.ja.butt@gmail.com>
2020-01-18 01:04:45 +1000

    Add memory usage to stats

--- bot/cogs/core/status.py
+++ bot/cogs/core/status.py
@@ -1,41 +1,56 @@
+import os
+
+import psutil
+
 import discord
 from discord.ext import commands
 
-from bot.utils import checks
+from bot.utils import checks, tools
 
 
 class Status(commands.Cog):
-    """Bot information commands."""
+    """Bot status information."""
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    async def cog_check(self, ctx: commands.Context) -> bool:
-        return await checks.is_owner(ctx)
-
     @commands.command(name='ping')
     async def ping(self, ctx: commands.Context):
-        """Determines the bots current latency"""
+        """Determines the bots current latency."""
         message = await ctx.send('Pong!')
         await message.edit(content=f'Pong! Latency: `{(message.created_at - ctx.message.created_at).total_seconds()}s`')
 
     @commands.command(name='status')
+    @commands.check(checks.is_owner)
     async def status(self, ctx: commands.Context):
-        """Shows some basic information about the bot's current status."""
+        """Sends some basic information about the bot."""
+
+        # Get lines of code
+        lines_of_code = os.popen(
+            r'find . -name "*.py" -exec cat {} \; | wc -l').read()
+
+        # Get memory usage
+        process = psutil.Process(os.getpid())
+        memory_usage = process.memory_info().rss / 1024 ** 2
+
         await ctx.send(
             embed=discord.Embed(
-                title=f'{self.bot.user.name} v{self.bot.__version__} Status:',
+                title=f'{self.bot.user.name} v{self.bot.__version__} Status',
                 colour=self.bot.user.colour
             ).set_thumbnail(
                 url=self.bot.user.avatar_url
             ).add_field(
-                name="Users:", value=len(self.bot.users)
+                name='Users:', value=len(self.bot.users)
             ).add_field(
-                name="Guilds:", value=len(self.bot.guilds)
+                name='Guilds:', value=len(self.bot.guilds)
             ).add_field(
-                name="Started at:", value=self.bot._start_time.strftime('%F %H:%M:%S UTC')
+                name='Started at:', value=tools.format_dt(self.bot._start_time)
             ).add_field(
-                name="Cogs loaded:", value=len(self.bot.cogs)
+                name='Memory usage:', value=f'{memory_usage:.2f} MB'
+            ).add_field(
+                name='Cogs loaded:', value=len(self.bot.cogs)
+            ).add_field(
+                name='Lines of code:', value=lines_of_code or 'Unknown'
             )
         )
 
