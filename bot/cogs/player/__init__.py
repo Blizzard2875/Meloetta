3eb60ee6bedb5fdfb981c1539574eb2b6731349f
Josh B <josh.ja.butt@gmail.com>
2020-04-09 19:20:15 +1000

    Fix check predicates for force

--- bot/cogs/player/__init__.py
+++ bot/cogs/player/__init__.py
@@ -323,7 +323,7 @@ class Player(commands.Cog):
 
     @tools.auto_help
     @commands.group(name='force')
-    @commands.check_any(checks.is_administrator, checks.is_owner)
+    @commands.check_any(commands.check(checks.is_administrator), commands.check(checks.is_owner))
     async def force(self, ctx):
         """Admin commands."""
         pass
