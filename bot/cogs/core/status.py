b138685f1c0c836250234dfa65cace8c872f3894
Josh <josh.ja.butt@gmail.com>
2020-08-14 20:52:32 +1000

    Ignore hidden files in code count

--- bot/cogs/core/status.py
+++ bot/cogs/core/status.py
@@ -74,7 +74,7 @@ class Status(commands.Cog):
 
         # Get lines of code
         lines_of_code = os.popen(
-            r'find . -name "*.py" -exec cat {} \; | wc -l').read()
+            r'find . -not -path "*/\.*" -name "*.py" -exec cat {} \; | wc -l').read()
 
         # Get memory usage
         process = psutil.Process(os.getpid())
