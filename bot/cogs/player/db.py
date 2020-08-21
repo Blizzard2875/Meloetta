19bf73e11d419b8155a75444f05f63de554d32b6
Josh <josh.ja.butt@gmail.com>
2020-08-21 23:23:15 +1000

    Fix typo

--- bot/cogs/player/db.py
+++ bot/cogs/player/db.py
@@ -3,5 +3,5 @@ from donphan import Column, SQLType, Table
 
 class Instances(Table):
     guild_id: SQLType.BigInt = Column(primary_key=True, index=True)
-    voice_channel_id: SQLType.BigInt = Column(nullablle=False)
+    voice_channel_id: SQLType.BigInt = Column(nullable=False)
     configuration: dict = Column(nullable=False, default={})
